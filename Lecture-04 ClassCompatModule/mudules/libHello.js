0c07f91438b8cd39065a27b4c38a36c24cafe5e9
noppakornn <noppakorn.5142@gmail.com>
2023-01-11 10:35:52 +0700

    update

---
+++ Lecture-04 ClassCompatModule/mudules/libHello.js
@@ -0,0 +1,19 @@
+//libHello.js
+const person ={
+    name: 'Anirach',
+    weight: 85
+}
+
+function sayHello(){
+    console.log("Hello World!")
+}
+
+function cube(x){
+    return x*x*x;
+}
+
+const add = (a,b) => a+b;
+
+const status = true;
+
+mudule.exports = {person, sayHello, cube, add, status}
